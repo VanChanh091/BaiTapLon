de058656f2c224f04d095482733ac4e84742e4e5
VanChanh <vanchanh0730@gmai.com>
2023-11-25 12:42:18 +0700

    json & img

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
       {/* <Stack.Navigator></Stack.Navigator> */}
-      {/* <User /> */}
-      <Notification />
+      <User />
+      {/* <Notification /> */}
     </NavigationContainer>
   );
 }
